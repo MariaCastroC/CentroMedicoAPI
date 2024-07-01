93eac1bb296902c7c8d0ec199e1d8f2dee5f09a3
MariaCastroC <alecalu525@hotmail.com>
2024-06-30 22:00:43 -0500

    apimaria

--- Controllers/MedicoController.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CentroMedicoAPI.Controllers
-{
-
-    //controladores la arquitectura sera MVC el back sera una API que expone servicios web y el front sera en HTML y js
-    public class MedicoController
-    {
-    }
-}
